ab8b1f4fee6419333c3dbf03a7d49b0d3094c4ac
pascal <pascal@vks.ai>
2022-01-10 20:21:37 +0100

    fix deploy

--- deploy.py
+++ deploy.py
@@ -8,7 +8,7 @@ commit_count = sh.git("rev-list", ["--all"]).count("\n")
 with open("setup.py") as f:
     setup = f.read()
 
-setup = re.sub("MICRO_VERSION = '[0-9]+'", "MICRO_VERSION = '{}'".format(commit_count), setup)
+setup = re.sub('MICRO_VERSION = "[0-9]+"', 'MICRO_VERSION = "{}"'.format(commit_count), setup)
 
 major = re.search('MAJOR_VERSION = "([0-9]+)"', setup).groups()[0]
 minor = re.search('MINOR_VERSION = "([0-9]+)"', setup).groups()[0]
