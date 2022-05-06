a9100c14fe20e41a03e1e8ee4d7aedf4dcbbdfa1
Anil Karatas <anil@mgnr.io>
2022-05-06 15:10:08 +0300

    reset micro version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 MAJOR_VERSION = "0"
 MINOR_VERSION = "11"
-MICRO_VERSION = "95"
+MICRO_VERSION = "0"
 VERSION = "{}.{}.{}".format(MAJOR_VERSION, MINOR_VERSION, MICRO_VERSION)
 
 setup(
