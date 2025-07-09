2f211ef2e4c2ef361fa1ae3c942e491120caa540
pascal <pascal@vks.ai>
2025-07-10 00:19:00 +0200

    try to switch to pyproject.toml

--- cliche/__init__.py
+++ cliche/__init__.py
@@ -1,5 +1,5 @@
 __project__ = "cliche"
-__version__ = "0.10.122"
+__version__ = "0.10.123"
 import sys
 import time
 import warnings
--- deploy.py
+++ deploy.py
@@ -1,23 +1,25 @@
-""" File unrelated to the package, except for convenience in deploying """
+"""File unrelated to the package, except for convenience in deploying"""
+
+import os
 import re
 import sys
+
 import sh
-import os
 
 commit_count = sh.git("rev-list", ["--all"]).count("\n")
 
-with open("setup.py") as f:
-    setup = f.read()
+with open("pyproject.toml") as f:
+    pyproject_content = f.read()
 
-setup = re.sub('MICRO_VERSION = "[0-9]+"', 'MICRO_VERSION = "{}"'.format(commit_count), setup)
+# Update version in pyproject.toml
+major = re.search(r'version = "(\d+)\.(\d+)\.(\d+)"', pyproject_content).groups()[0]
+minor = re.search(r'version = "(\d+)\.(\d+)\.(\d+)"', pyproject_content).groups()[1]
+version = f"{major}.{minor}.{commit_count}"
 
-major = re.search('MAJOR_VERSION = "([0-9]+)"', setup).groups()[0]
-minor = re.search('MINOR_VERSION = "([0-9]+)"', setup).groups()[0]
-micro = re.search('MICRO_VERSION = "([0-9]+)"', setup).groups()[0]
-version = "{}.{}.{}".format(major, minor, micro)
+pyproject_content = re.sub(r'version = "\d+\.\d+\.\d+"', f'version = "{version}"', pyproject_content)
 
-with open("setup.py", "w") as f:
-    f.write(setup)
+with open("pyproject.toml", "w") as f:
+    f.write(pyproject_content)
 
 name = os.getcwd().split("/")[-1]
 
@@ -25,8 +27,8 @@ with open(f"{name}/__init__.py") as f:
     init = f.read()
 
 with open(f"{name}/__init__.py", "w") as f:
-    f.write(re.sub('__version__ = "[0-9.]+"', '__version__ = "{}"'.format(version), init))
+    f.write(re.sub('__version__ = "[0-9.]+"', f'__version__ = "{version}"', init))
 
 os.system("rm -rf dist/")
-os.system(f"{sys.executable} setup.py sdist bdist_wheel")
+os.system(f"{sys.executable} -m build")
 os.system("twine upload dist/*")
